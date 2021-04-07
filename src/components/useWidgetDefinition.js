07037ff96e4623c70d410bb785ee36c1bb1d83b8
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-04-07 13:53:35 +0100

    added explanatory comment

--- src/components/useWidgetDefinition.js
+++ src/components/useWidgetDefinition.js
@@ -4,6 +4,11 @@ import { useQuery } from 'react-query';
 
 import getComponentsFromType from './getComponentsFromType';
 
+
+/* Hook to expose the definition and consequently
+ * the components relevant to that def's type
+ * from the definition id
+ */
 const useWidgetDefinition = (defId) => {
   const ky = useOkapiKy();
   /*
