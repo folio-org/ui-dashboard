cfada68c780fb8ab677663a0baff8da046d0e7e1
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-04-08 17:03:30 +0100

    small code cleanup

--- src/components/TypeComponents/SimpleSearch/Form/sort/SimpleSearchSort.js
+++ src/components/TypeComponents/SimpleSearch/Form/sort/SimpleSearchSort.js
@@ -5,7 +5,6 @@ import { FormattedMessage } from 'react-intl';
 import { Field, useForm, useFormState } from 'react-final-form';
 import {
   Col,
-  KeyValue,
   Select
 } from '@folio/stripes/components';
 
@@ -47,14 +46,12 @@ const SimpleSearchSort = ({ data: { sortColumns } = {} }) => {
     return (
       <>
         <Field
-          defaultValue={sortColumns[0].name}
           name="sortColumn.name"
           render={() => {
             return null;
           }}
         />
         <Field
-          defaultValue={sortColumns[0].sortTypes[0]}
           name="sortColumn.sortType"
           render={() => {
             return null;
@@ -70,38 +67,30 @@ const SimpleSearchSort = ({ data: { sortColumns } = {} }) => {
   return (
     <>
       <Col xs={3}>
-        <KeyValue
-          data-testid="simple-search-sort-sort-by"
+        <Field
+          component={Select}
+          dataOptions={selectifiedSortColumns}
           label={<FormattedMessage id="ui-dashboard.simpleSearchForm.sort.sortBy" />}
-        >
-          <Field
-            component={Select}
-            dataOptions={selectifiedSortColumns}
-            name="sortColumn.name"
-            onChange={e => {
-              setSSC(sortColumns.find(sc => sc.name === e.target.value));
-              /*
-                * This means you can't set sort order THEN sortBy,
-                * but this is because in the definition sort direction belongs to the sortBy
-              */
-              change('sortColumn.sortType', selectedSortCol.sortTypes[0]);
-              change('sortColumn.name', e.target.value);
-            }}
-          />
-        </KeyValue>
+          name="sortColumn.name"
+          onChange={e => {
+            setSSC(sortColumns.find(sc => sc.name === e.target.value));
+            /*
+              * This means you can't set sort order THEN sortBy,
+              * but this is because in the definition sort direction belongs to the sortBy
+            */
+            change('sortColumn.sortType', selectedSortCol.sortTypes[0]);
+            change('sortColumn.name', e.target.value);
+          }}
+        />
       </Col>
       <Col xs={3}>
-        <KeyValue
-          data-testid="simple-search-sort-sort-direction"
+        <Field
+          component={Select}
+          dataOptions={selectifiedSortDirs}
+          disabled={selectifiedSortDirs.length <= 1}
           label={<FormattedMessage id="ui-dashboard.simpleSearchForm.sort.sortDirection" />}
-        >
-          <Field
-            component={Select}
-            dataOptions={selectifiedSortDirs}
-            disabled={selectifiedSortDirs.length <= 1}
-            name="sortColumn.sortType"
-          />
-        </KeyValue>
+          name="sortColumn.sortType"
+        />
       </Col>
     </>
   );
