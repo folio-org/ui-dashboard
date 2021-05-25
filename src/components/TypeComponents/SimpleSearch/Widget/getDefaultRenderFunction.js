31b536d3b3b7dfb95ce63f324f8e2d51f0787d38
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-05-25 12:52:33 +0100

    moved search for registry specific render function inside getDefaulRenderFunction

--- src/components/TypeComponents/SimpleSearch/Widget/getDefaultRenderFunction.js
+++ src/components/TypeComponents/SimpleSearch/Widget/getDefaultRenderFunction.js
@@ -1,38 +1,68 @@
 import React from 'react';
-import { get } from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { FormattedUTCDate, Icon, NoValue } from '@folio/stripes/components';
+
+import Registry from '../../../../Registry'
 
 /*
   Takes in a simpleSearch result->column shape
   and returns a default renderFunction for that shape, of the form
   (entireRecord) => instructions_to_render_specific_field
 */
-const getDefaultRenderFunction = ({ accessPath, valueType }) => {
+const getDefaultRenderFunction = ({ accessPath, name: drcName, valueType }, resource) => {
+  // First attempt to get Registry render function
+  const regFunc = Registry.getRenderFunction(resource, drcName);
+  if (regFunc) {
+    return regFunc;
+  }
+
+  // If that does not exist, create default render function
   if (accessPath && valueType) {
+    console.log(`accessPath: ${accessPath}, valueType: ${valueType}`)
     switch (valueType.toLowerCase()) {
       case 'date': {
         return (data) => {
-          const date = get(data, accessPath);
+          const date = data[accessPath];
           return date ? <FormattedUTCDate value={date} /> :
           <NoValue />;
         };
       }
       case 'boolean': {
         return (data) => {
-          const bool = get(data, accessPath);
+          const bool = data[accessPath];
           return bool ? <Icon icon="check-circle" /> :
           <Icon icon="times-circle" />;
         };
       }
+      case 'link': {
+        return (data) => {
+          const linkText = data[accessPath];
+          const viewTemplate = Registry.getResource(resource)?.getViewTemplate()
+          console.log(`ViewTemplate for ${resource}: ${viewTemplate}`)
+          
+          if (!viewTemplate) {
+            return linkText;
+          }
+
+          return (
+            <Link
+              to={viewTemplate(data)}
+            >
+              {linkText}
+            </Link>
+          );
+        };
+      }
       default: {
-        return (data) => get(data, accessPath);
+        return (data) => data[accessPath];
       }
     }
   } else {
     // No accessPath, just return full data as string (Will probably show as "[Object object]")
     return (data) => data.toString();
   }
+
 };
 
 export default getDefaultRenderFunction;
