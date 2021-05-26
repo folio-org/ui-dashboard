44008423d14f83fa9d0027a49c445c7a30d7976c
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-05-26 12:58:49 +0100

    linting and changelog update

--- src/components/TypeComponents/SimpleSearch/Widget/simpleSearchPathBuilder.test.js
+++ src/components/TypeComponents/SimpleSearch/Widget/simpleSearchPathBuilder.test.js
@@ -12,223 +12,223 @@ const stripes = {
 };
 
 const widgetDef = {
-  "baseUrl":"/erm/sas",
-  "filters": {
-    "columns": [
+  'baseUrl':'/erm/sas',
+  'filters': {
+    'columns': [
       {
-        "name":"agreementName",
-        "label": "Agreement name",
-        "filterPath":"name",
-        "valueType": "String",
-        "comparators": ["==", "!=", "=~", "!~"]
+        'name':'agreementName',
+        'label': 'Agreement name',
+        'filterPath':'name',
+        'valueType': 'String',
+        'comparators': ['==', '!=', '=~', '!~']
       },
       {
-        "name":"agreement",
-        "label": "Agreement",
-        "filterPath":"id",
-        "valueType": "UUID",
-        "resource": "agreement",
-        "comparators": ["==", "!="]
+        'name':'agreement',
+        'label': 'Agreement',
+        'filterPath':'id',
+        'valueType': 'UUID',
+        'resource': 'agreement',
+        'comparators': ['==', '!=']
       },
       {
-        "name":"agreementStatus",
-        "label": "Agreement status",
-        "filterPath":"agreementStatus.value",
-        "valueType": "Enum",
-        "enumValues": [
-          {"value": "active", "label": "Active"},
-          {"value": "closed", "label": "Closed"},
-          {"value": "draft", "label": "Draft"},
-          {"value": "in_negotiation", "label": "In negotiation"},
-          {"value": "requested", "label": "Requested"}
+        'name':'agreementStatus',
+        'label': 'Agreement status',
+        'filterPath':'agreementStatus.value',
+        'valueType': 'Enum',
+        'enumValues': [
+          { 'value': 'active', 'label': 'Active' },
+          { 'value': 'closed', 'label': 'Closed' },
+          { 'value': 'draft', 'label': 'Draft' },
+          { 'value': 'in_negotiation', 'label': 'In negotiation' },
+          { 'value': 'requested', 'label': 'Requested' }
         ],
-        "comparators": ["==", "!="]
+        'comparators': ['==', '!=']
       },
       {
-        "name":"startDate",
-        "label": "Start date",
-        "filterPath":"startDate",
-        "valueType": "Date",
-        "comparators": ["==", "!=", ">=", "<=", "isSet", "isNotSet"]
+        'name':'startDate',
+        'label': 'Start date',
+        'filterPath':'startDate',
+        'valueType': 'Date',
+        'comparators': ['==', '!=', '>=', '<=', 'isSet', 'isNotSet']
       },
       {
-        "name":"endDate",
-        "label": "End date",
-        "filterPath":"endDate",
-        "valueType": "Date",
-        "comparators": ["==", "!=", ">=", "<=", "isSet", "isNotSet"]
+        'name':'endDate',
+        'label': 'End date',
+        'filterPath':'endDate',
+        'valueType': 'Date',
+        'comparators': ['==', '!=', '>=', '<=', 'isSet', 'isNotSet']
       },
       {
-        "name":"cancellationDeadline",
-        "label": "Cancellation deadline",
-        "filterPath":"cancellationDeadline",
-        "valueType": "Date",
-        "comparators": ["==", "!=", ">=", "<=", "isSet", "isNotSet"]
+        'name':'cancellationDeadline',
+        'label': 'Cancellation deadline',
+        'filterPath':'cancellationDeadline',
+        'valueType': 'Date',
+        'comparators': ['==', '!=', '>=', '<=', 'isSet', 'isNotSet']
       },
       {
-        "name":"internalContact",
-        "label": "Internal contact",
-        "filterPath":"contacts.user.id",
-        "valueType": "UUID",
-        "resource": "user",
-        "comparators": ["==", "!="]
+        'name':'internalContact',
+        'label': 'Internal contact',
+        'filterPath':'contacts.user.id',
+        'valueType': 'UUID',
+        'resource': 'user',
+        'comparators': ['==', '!=']
       },
       {
-        "name":"tags",
-        "label": "Tags",
-        "filterPath":"tags.value",
-        "valueType": "String",
-        "comparators": ["==", "!=", "=~", "!~"]
+        'name':'tags',
+        'label': 'Tags',
+        'filterPath':'tags.value',
+        'valueType': 'String',
+        'comparators': ['==', '!=', '=~', '!~']
       },
       {
-        "name":"isPerpetual",
-        "label": "Is perpetual",
-        "filterPath":"isPerpetual.value",
-        "valueType": "Enum",
-        "enumValues": [
-          {"value": "yes", "label": "Yes"},
-          {"value": "no", "label": "No"}
+        'name':'isPerpetual',
+        'label': 'Is perpetual',
+        'filterPath':'isPerpetual.value',
+        'valueType': 'Enum',
+        'enumValues': [
+          { 'value': 'yes', 'label': 'Yes' },
+          { 'value': 'no', 'label': 'No' }
         ],
-        "comparators": ["==", "!="]
+        'comparators': ['==', '!=']
       },
       {
-        "name": "hasItems",
-        "label": "Has agreement lines",
-        "filterPath": "items",
-        "valueType": "Array",
-        "comparators": ["isEmpty", "isNotEmpty"]
+        'name': 'hasItems',
+        'label': 'Has agreement lines',
+        'filterPath': 'items',
+        'valueType': 'Array',
+        'comparators': ['isEmpty', 'isNotEmpty']
       }
     ]
   },
-  "sort": {
-    "columns": [
+  'sort': {
+    'columns': [
       {
-        "name":"id",
-        "label": "Id",
-        "sortPath":"id",
-        "sortTypes": ["asc", "desc"]
+        'name':'id',
+        'label': 'Id',
+        'sortPath':'id',
+        'sortTypes': ['asc', 'desc']
       },
       {
-        "name":"agreementName",
-        "label": "Name",
-        "sortPath":"name",
-        "sortTypes": ["asc", "desc"]
+        'name':'agreementName',
+        'label': 'Name',
+        'sortPath':'name',
+        'sortTypes': ['asc', 'desc']
       },
       {
-        "name":"agreementStatus",
-        "label": "Status",
-        "sortPath":"agreementStatus.label",
-        "sortTypes": ["asc", "desc"]
+        'name':'agreementStatus',
+        'label': 'Status',
+        'sortPath':'agreementStatus.label',
+        'sortTypes': ['asc', 'desc']
       },
       {
-        "name":"startDate",
-        "sortPath":"startDate",
-        "label": "Start date",
-        "sortTypes": ["asc", "desc"]
+        'name':'startDate',
+        'sortPath':'startDate',
+        'label': 'Start date',
+        'sortTypes': ['asc', 'desc']
       },
       {
-        "name":"endDate",
-        "sortPath":"endDate",
-        "label": "End date",
-        "sortTypes": ["asc", "desc"]
+        'name':'endDate',
+        'sortPath':'endDate',
+        'label': 'End date',
+        'sortTypes': ['asc', 'desc']
       },
       {
-        "name":"cancellationDeadline",
-        "sortPath":"cancellationDeadline",
-        "label": "Cancellation deadline",
-        "sortTypes": ["asc", "desc"]
+        'name':'cancellationDeadline',
+        'sortPath':'cancellationDeadline',
+        'label': 'Cancellation deadline',
+        'sortTypes': ['asc', 'desc']
       }
     ]
   }
-}
+};
 
 const widgetConfSort = {
-  "sortColumn": {
-    "name": "id",
-    "sortType": "asc"
+  'sortColumn': {
+    'name': 'id',
+    'sortType': 'asc'
   }
-}
+};
 
 const widgetConfStatus = {
-  "sortColumn": {
-    "name": "id",
-    "sortType": "asc"
+  'sortColumn': {
+    'name': 'id',
+    'sortType': 'asc'
   },
-  "filterColumns": [{
-    "name": "agreementStatus",
-    "rules": [{
-      "comparator": "==",
-      "filterValue": "draft"
+  'filterColumns': [{
+    'name': 'agreementStatus',
+    'rules': [{
+      'comparator': '==',
+      'filterValue': 'draft'
     }]
   }]
-}
+};
 
 const widgetConfStatusTwoValues = {
-  "sortColumn": {
-    "name": "id",
-    "sortType": "asc"
+  'sortColumn': {
+    'name': 'id',
+    'sortType': 'asc'
   },
-  "filterColumns": [{
-    "name": "agreementStatus",
-    "rules": [{
-      "comparator": "==",
-      "filterValue": "draft"
+  'filterColumns': [{
+    'name': 'agreementStatus',
+    'rules': [{
+      'comparator': '==',
+      'filterValue': 'draft'
     }, {
-      "comparator": "!=",
-      "filterValue": "closed"
+      'comparator': '!=',
+      'filterValue': 'closed'
     }]
   }]
-}
+};
 
 const widgetConfStatusAndName = {
-  "sortColumn": {
-    "name": "id",
-    "sortType": "asc"
+  'sortColumn': {
+    'name': 'id',
+    'sortType': 'asc'
   },
-  "filterColumns": [{
-    "name": "agreementStatus",
-    "rules": [{
-      "comparator": "==",
-      "filterValue": "draft"
+  'filterColumns': [{
+    'name': 'agreementStatus',
+    'rules': [{
+      'comparator': '==',
+      'filterValue': 'draft'
     }]
   },
   {
-    "name": "agreementName",
-    "rules": [{
-      "comparator": "==",
-      "filterValue": "wibble"
+    'name': 'agreementName',
+    'rules': [{
+      'comparator': '==',
+      'filterValue': 'wibble'
     }]
   }]
-}
+};
 
 const widgetConfSpecialComparator = {
-  "filterColumns": [{
-    "name": "cancellationDeadline",
-    "rules": [{
-      "comparator": "isNotSet"
+  'filterColumns': [{
+    'name': 'cancellationDeadline',
+    'rules': [{
+      'comparator': 'isNotSet'
     }]
   }]
-}
+};
 
 const widgetConfToday = {
-  "filterColumns": [{
-    "name": "startDate",
-    "rules": [{
-      "comparator": "==",
-      "filterValue": "{{currentDate}}"
+  'filterColumns': [{
+    'name': 'startDate',
+    'rules': [{
+      'comparator': '==',
+      'filterValue': '{{currentDate}}'
     }]
   }]
-}
+};
 
 const widgetConfMe = {
-  "filterColumns": [{
-    "name": "internalContact",
-    "rules": [{
-      "comparator": "!=",
-      "filterValue": "{{currentUser}}"
+  'filterColumns': [{
+    'name': 'internalContact',
+    'rules': [{
+      'comparator': '!=',
+      'filterValue': '{{currentUser}}'
     }]
   }]
-}
+};
 
 describe('simpleSearchPathBuilder', () => {
   test('simpleSearchPathBuilder function works as expected for just sort', () => {
@@ -257,13 +257,13 @@ describe('simpleSearchPathBuilder', () => {
   });
 
   test('simpleSearchPathBuilder function works as expected with date tokens', () => {
-    const today = tokens("{{currentDate}}", stripes)
+    const today = tokens('{{currentDate}}', stripes);
     const output = simpleSearchPathBuilder(widgetDef, widgetConfToday, stripes);
     expect(output).toBe(`erm/sas?filters=startDate==${today}&stats=true`);
   });
 
   test('simpleSearchPathBuilder function works as expected with user tokens', () => {
-    const me = tokens("{{currentUser}}", stripes)
+    const me = tokens('{{currentUser}}', stripes);
     const output = simpleSearchPathBuilder(widgetDef, widgetConfMe, stripes);
     expect(output).toBe(`erm/sas?filters=contacts.user.id!=${me}&stats=true`);
   });
