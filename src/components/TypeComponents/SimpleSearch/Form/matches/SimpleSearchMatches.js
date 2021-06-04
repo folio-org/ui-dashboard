6acf2e121a52fc06676c33aa20d8a7ea614faa3b
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-06-04 11:47:45 +0100

    added SimpleSearchMatches component

---
+++ src/components/TypeComponents/SimpleSearch/Form/matches/SimpleSearchMatches.js
@@ -0,0 +1,35 @@
+import { FormattedMessage } from 'react-intl';
+
+import { Field, useFormState } from 'react-final-form';
+
+import {
+  Accordion,
+  SearchField,
+} from '@folio/stripes/components';
+import { match } from 'sinon';
+
+const SimpleSearchMatches = ({
+  data: {
+    matches
+  },
+  id
+}) => {
+  const { values } = useFormState();
+  console.log('Matches: %o', matches);
+  console.log('Values: %o', values);
+
+  return (
+    // TODO, deal with this mess.... Stripes components not doing what they say they should
+    <Accordion
+      id={id}
+      label={<FormattedMessage id="ui-dashboard.simpleSearchForm.matches" />}
+    >
+      <SearchField
+        defaultValue={match.defaultTerm}
+        name="matches"
+      />
+    </Accordion>
+  );
+};
+
+export default SimpleSearchMatches;
