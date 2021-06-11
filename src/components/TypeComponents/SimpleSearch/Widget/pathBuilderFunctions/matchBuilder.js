1ab6bf61454f51cea78723360dc84a348149110e
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-06-11 11:54:53 +0100

    split pathBuilder function into multiple child functions

---
+++ src/components/TypeComponents/SimpleSearch/Widget/pathBuilderFunctions/matchBuilder.js
@@ -0,0 +1,49 @@
+/*
+    matches will be an object of the form:
+      {
+        term: "abcde",
+        matches: {
+          <matchColName>: true,
+          <matchColName>: false,
+          <matchColName>: true,
+          ...
+        }
+      }
+      where each matchCol name comes from the widgetDefinition.
+  */
+
+const matchBuilder = (matches, defMatchColumns) => {
+  let matchString = '';
+  if (matches) {
+    const {
+      term,
+      matches: matchObject
+    } = matches;
+    // Start building the matchString
+
+    // Ensure there is a term before continuing
+    if (term?.trim()) {
+      for (const [key, value] of Object.entries(matchObject)) {
+        // [key,value] should look like [agreementName, true] or [description, false]
+        // Only act on match if configured
+        if (value) {
+          const matchColumn = defMatchColumns.find(dmc => dmc.name === key);
+          // If we can't find the column in the def then ignore
+          if (matchColumn) {
+            matchString += `${matchString.length ? '&' : ''}match=${matchColumn.accessPath}`;
+          }
+        }
+      }
+
+      // Don't bother adding match if there are no match columns
+      if (matchString.length) {
+        matchString += `&term=${term}`;
+      }
+    }
+  }
+
+  return matchString;
+};
+
+export default matchBuilder;
+
--- src/components/TypeComponents/SimpleSearch/Widget/simpleSearchPathBuilder.js
+++ src/components/TypeComponents/SimpleSearch/Widget/simpleSearchPathBuilder.js
@@ -1,5 +1,6 @@
-import tokens from '../../../../tokens';
-import isComparatorSpecialCase from '../../utilities';
+import matchBuilder from './pathBuilderFunctions/matchBuilder';
+import filterBuilder from './pathBuilderFunctions/filterBuilder';
+import sortBuilder from './pathBuilderFunctions/sortBuilder';
 
 const simpleSearchPathBuilder = (widgetDef, widgetConf, stripes) => {
   const {
@@ -32,150 +33,25 @@ const simpleSearchPathBuilder = (widgetDef, widgetConf, stripes) => {
   } = widgetConf;
   pathString += '?';
 
-  /*
-    matches will be an obvject of the form:
-      {
-        term: "abcde",
-        matches: {
-          <matchColName>: true,
-          <matchColName>: false,
-          <matchColName>: true,
-          ...
-        }
-      }
-      where each matchCol name comes from the widgetDefinition.
-  */
+  const matchString = matchBuilder(matches, defMatchColumns);
+  pathString += matchString;
 
-  if (matches) {
-    const {
-      term,
-      matches: matchObject
-    } = matches;
-    // Start building the matchString
-    let matchString = '';
+  const filterString = filterBuilder(filterColumns, defFilterColumns, stripes);
+  pathString += `${matchString.length ? '&' : ''}${filterString}`;
 
-    // Ensure there is a term before continuing
-    if (term.trim()) {
-      for (const [key, value] of Object.entries(matchObject)) {
-        // [key,value] should look like [agreementName, true] or [description, false]
-        // Only act on match if configured
-        if (value) {
-          const matchColumn = defMatchColumns.find(dmc => dmc.name === key);
-          // If we can't find the column in the def then ignore
-          if (matchColumn) {
-            matchString += `${matchString.length ? '&' : ''}match=${matchColumn.accessPath}`;
-          }
-        }
-      }
+  const sortString = sortBuilder(sortColumn, defSortColumns);
+  pathString += `${(matchString.length || filterString.length) ? '&' : ''}${sortString}`;
 
-      // Don't bother adding match if there are no match columns
-      if (matchString.length) {
-        matchString += `&term=${term}`;
-      }
-    }
-    pathString += matchString;
-  }
-
-  /*
-      filterColumns will be an array of the form:
-      [
-        {
-          name: "agreementStatus",
-          rules: [
-            {
-              comparator: "==",
-              filterValue: "active"
-            },
-            {
-              comparator: "==",
-              filterValue: "closed"
-            }
-          ]
-        },
-        {
-          name: "startDate",
-          rules: [
-            {
-              comparator: ">",
-              filterValue: "2012-02-01"
-            }
-          ]
-        },
-        {
-          name: "startDate",
-          rules: [
-            {
-              comparator: "<",
-              filterValue: "2021-02-22"
-            }
-          ]
-        }
-      ]
-      We need to AND the top level filters, and OR the second level rules
-    */
-
-  if (filterColumns) {
-    // Start building the filterString
-    let filterString = '';
-    if (matches) {
-      filterString += '&';
-    }
-
-    // Begin each filter with & unless it's the first one
-    filterColumns.forEach((f, index) => {
-      let specificFilterString = '';
-      if (index !== 0) {
-        specificFilterString = '&filters=';
-      } else {
-        specificFilterString = 'filters=';
-      }
-
-      // This assumes that if a filterColumn exists then that column will always be in the widgetDef
-      // We need to implement some kind of auto-schema check on the backend to support this
-      const filterPath = (defFilterColumns.find(fc => fc.name === f.name))?.filterPath;
-
-      // Then take each of the rules within the filter, and OR them together with the correct comparators
-      const { rules } = f;
-      rules.forEach((r, ind) => {
-        if (isComparatorSpecialCase(r.comparator)) {
-          // If we're allowing null the filterString is slightly different
-          specificFilterString += `${filterPath}%20${r.comparator}`;
-        } else {
-          // Ensure we're safely encoding all special characters into the filters path, after applying tokens
-          const encodedFilterValue = encodeURI(tokens(r.filterValue, stripes));
-          specificFilterString += `${filterPath}${r.comparator ?? '=='}${encodedFilterValue}`;
-        }
-        if (ind !== rules.length - 1) {
-          // This doesn't work as "||", it needs encoded value
-          specificFilterString += '%7C%7C';
-        }
-      });
-      filterString += specificFilterString;
-    });
-    pathString += filterString;
-  }
-
-  if (sortColumn) {
-    // Start building the sortString
-    let sortString = '';
-
-    // Check we have something other than just sort
-    if (pathString !== '?') {
-      sortString += '&';
-    }
-    sortString += 'sort=';
-    // At this point we should have either '&sort=' or 'sort='
-    const sortPath = (defSortColumns.find(sc => sc.name === sortColumn.name))?.sortPath;
-    sortString += `${sortPath};${sortColumn.sortType}`;
-
-    pathString += sortString;
-  }
-
-  if (filterColumns || sortColumn) {
-    pathString += '&stats=true';
-  } else {
-    pathString += 'stats=true';
-  }
+  // Add stats=true
+  pathString += `${
+    (
+      matchString.length ||
+      filterString.length ||
+      sortString.length
+    ) ?
+      '&' :
+      ''
+  }stats=true`;
 
   if (numberOfRows) {
     // We can assume always & because stats will be present
