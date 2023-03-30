56dc0176449a840f3bcc4c33580fded0d93618f5
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-03-30 12:38:34 +0200

    SI-12 Potential CROSS SITE SCRIPTING (XSS) vulnerability
    * remove comment

--- src/components/TypeComponents/utilities/index.js
+++ src/components/TypeComponents/utilities/index.js
@@ -1,3 +1,2 @@
 export { default as isComparatorSpecialCase } from './isComparatorSpecialCase';
 export { default as validateURL } from './validators';
-// export { default as validateURLIsValid } from './validators';
