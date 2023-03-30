3f6780ca4fcf4673dd891b336b9854ac51c0242e
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-03-30 11:35:49 +0200

    SI-12 Potential CROSS SITE SCRIPTING (XSS) vulnerability
    * add tooltip

--- src/components/TypeComponents/utilities/validators.js
+++ src/components/TypeComponents/utilities/validators.js
@@ -1,11 +1,27 @@
 import { FormattedMessage } from 'react-intl';
+import { Tooltip, IconButton } from '@folio/stripes/components';
 
 const validateURL = (value) => {
   if (value) {
     const whiteList = ['/', 'http://', 'https://'];
     const blackList = /[<>"'\\{}|^[\]`%;?@=#!]/;
     if (!whiteList.some(pattern => value.startsWith(pattern)) || value.match(blackList)) {
-      return <FormattedMessage id="ui-dashboard.simpleSearchForm.error.invalidURL" />;
+      return (
+        <><FormattedMessage id="ui-dashboard.simpleSearchForm.error.invalidURL" />
+          <Tooltip
+            id="tooltipValidateUrl"
+            text={<FormattedMessage id="ui-dashboard.simpleSearchForm.error.invalidURL.tooltip" />}
+          >
+            {({ ref, ariaIds }) => (
+              <IconButton
+                ref={ref}
+                aria-labelledby={ariaIds.text}
+                icon="exclamation-circle"
+              />
+            )}
+          </Tooltip>
+        </>
+      );
     } else {
       // eslint-disable-next-line no-param-reassign
       value = encodeURI(value);
