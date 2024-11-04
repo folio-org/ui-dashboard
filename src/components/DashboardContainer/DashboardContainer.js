434bbb760632d1d72ac313db6b6a6ebc14114239
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-11-04 10:58:45 +0000

    style: CSS tweaks to prevent bars going off screen

--- src/components/DashboardContainer/DashboardContainer.js
+++ src/components/DashboardContainer/DashboardContainer.js
@@ -100,10 +100,10 @@ const DashboardContainer = ({
           onReorder={widgets?.length > 1 ? onReorder : null}
           onUserAccess={onUserAccess}
         />
-        <div className={css.dashboardContentBanner}>
-          <DashboardAccessInfo dashId={dashboard.id} />
+        <>
+          <div className={css.dashboardBannerContainer}><DashboardAccessInfo dashId={dashboard.id} /></div>
           <div className={css.dashboardContent}> {dashboardContents()} </div>
-        </div>
+        </>
         <ConfirmationModal
           buttonStyle="danger"
           confirmLabel={<FormattedMessage id="ui-dashboard.delete" />}
