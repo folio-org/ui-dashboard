01a186cff38a73e272bcbe8553ca276388aa204e
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-08-15 13:49:25 +0100

    build: Bump Zustand dependency to ^5.0.0

--- src/hooks/useDashboardAccess.js
+++ src/hooks/useDashboardAccess.js
@@ -1,13 +1,15 @@
-import { shallow } from 'zustand/shallow';
+import { useShallow } from 'zustand/shallow';
 import useDashboardAccessStore from './useDashboardAccessStore';
 
 // Export these in one go, to make imports slighly nicer
 const useDashboardAccess = (dashId) => {
-  return useDashboardAccessStore(state => ({
-    access: state.dashboards[dashId]?.access,
-    hasAccess: state.dashboards[dashId]?.hasAccess ?? (() => null),
-    hasAdminPerm: state.dashboards[dashId]?.hasAdminPerm,
-  }), shallow);
+  return useDashboardAccessStore(
+    useShallow(state => ({
+      access: state.dashboards[dashId]?.access,
+      hasAccess: state.dashboards[dashId]?.hasAccess ?? (() => null),
+      hasAdminPerm: state.dashboards[dashId]?.hasAdminPerm,
+    }))
+  );
 };
 
 export default useDashboardAccess;
